e9821490a9094ec72ea87e0c73a6d027da596354
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2025-03-26 12:20:37 +0100

    Add schemas and migrate to DB

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,7 +1,21 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { doublePrecision, integer, pgTable, varchar } from "drizzle-orm/pg-core";
 
 export const usersTable = pgTable("users", {
   id: integer().primaryKey().generatedAlwaysAsIdentity(),
   name: varchar({ length: 255 }).notNull(),
   email: varchar({ length: 255 }).notNull().unique(),
+});
+
+export const categoryTable = pgTable("categories", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  FKUserID: integer("FK_user_ID").references(()=>usersTable.id, {onDelete: 'cascade'}),
+  name: varchar({ length: 255 }).notNull(),
+  
+});
+
+export const expenses = pgTable("expenses", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  FKCategoryID: integer("FK_category_ID").references(()=>categoryTable.id, {onDelete: 'cascade'}),
+  name: varchar({ length: 255 }).notNull(),
+  value: doublePrecision(),
 });
