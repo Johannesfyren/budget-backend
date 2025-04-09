57da66ae8e9b9884f47647c48ec9727d9c460148
Johannes Mac <159946573+Johannesfyren@users.noreply.github.com>
2025-04-09 15:20:43 +0200

    Add TypeScript type user

--- src/controllers/setupData.ts
+++ src/controllers/setupData.ts
@@ -4,6 +4,7 @@ import { Request, Response } from "express";
 const C_getAllCategoriesFromSections = async (req: Request, res: Response) => {
 	try {
 		const sectionID = req.params.sectionID;
+		if (req.user == undefined) return res.sendStatus(403);
 
 		const data = await db.getAllCategoriesFromSections(
 			Number(sectionID),
